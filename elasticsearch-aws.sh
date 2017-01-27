97efcc04ee4021a2ea9f4665c23f2d5a42912e2a
David Hallum <david@driveclutch.com>
2017-01-27 09:31:02 -0500

    Adding cluster.name as a configurable property [#138492883]

--- elasticsearch-aws.sh
+++ elasticsearch-aws.sh
@@ -9,10 +9,12 @@ SG=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/${M
 IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 
 CLIENT_PORT=${CLIENT_PORT:-9200}
+CLUSTER_NAME=${CLUSTER_NAME:-unknowncluster}
 
 chown elasticsearch /usr/share/elasticsearch/data
 
 gosu elasticsearch elasticsearch \
+  -E cluster.name=${CLUSTER_NAME} \
   -E network.host=${IP} \
   -E http.port=${CLIENT_PORT} \
   -E discovery.type=ec2 \
