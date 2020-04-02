cbea122c7dc87543dc6882502f1d49e40b2bd7ae
pwalters04 <pwalters2004@gmail.com>
2020-04-02 10:21:47 -0400

    added a bucket

--- elasticsearch-aws.sh
+++ elasticsearch-aws.sh
@@ -7,16 +7,6 @@ REGION=${AZ:0:${#AZ}-1}
 MAC=$(curl -s http://169.254.169.254/latest/meta-data/mac)
 SG=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/${MAC}/security-groups)
 IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-S3REPOSETTINGS="'{
-   "type":"s3",
-   "settings":{
-      "bucket":"com.driveclutch.production.es-index-storage",
-      "base_path":"telematics",
-      "access_key":"ACCESS_KEY",
-      "secret_key":"SECRET_KEY"
-   }
-}
-'"
 
 CLIENT_PORT=${CLIENT_PORT:-9200}
 
@@ -28,6 +18,8 @@ gosu elasticsearch elasticsearch \
   --discovery.type=ec2 \
   --discovery.ec2.groups=${SG} \
   --cloud.aws.region=${REGION} \
-  --cloud.aws.protocol=https
+  --cloud.aws.protocol=https \
+  --cloud.aws.access_key=${ACCESS_KEY} \
+  --cloud.aws.secret_key=${SECRET_KEY} \
+  --repositories.s3.bucket=com.driveclutch.production.es-index-storage
 
-curl -X PUT localhost:${CLIENT_PORT}/_snapshot/my_s3_repository?pretty -H "Content-Type: application/json" -d "${S3REPOSETTINGS}"
